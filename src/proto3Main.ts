734e81bceb04076b98f8db596c830dbbd814326e
catusax <root@coolrc.me>
2023-06-08 17:44:53 +0800

    feat: format with `buf format`

--- src/proto3Main.ts
+++ src/proto3Main.ts
@@ -88,28 +88,46 @@ export function activate(ctx: vscode.ExtensionContext): void {
     });
 
     vscode.languages.registerDocumentFormattingEditProvider('proto3', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            let args = [];
-            let opts = { input: document.getText() };
+        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.ProviderResult<vscode.TextEdit[]> {
 
-            // In order for clang-format to find the correct formatting file we need to have cwd set appropriately
-            switch (document.uri.scheme) {
-                case "untitled": // File has not yet been saved to disk use workspace path
-                    opts['cwd'] = vscode.workspace.rootPath;
-                    args.push(`--assume-filename=untitled.proto`);
-                    break;
-                case "file": // File is on disk use it's directory
-                    opts['cwd'] = path.dirname(document.uri.fsPath);
-                    args.push(`--assume-filename=${document.uri.fsPath}`);
-                    break;
+            switch (Proto3Configuration.Instance().formatter()) {
+                case 'clang-format':
+                    let args = [];
+                    let opts = { input: document.getText() };
+                    // In order for clang-format to find the correct formatting file we need to have cwd set appropriately
+                    switch (document.uri.scheme) {
+                        case "untitled": // File has not yet been saved to disk use workspace path
+                            opts['cwd'] = vscode.workspace.rootPath;
+                            args.push(`--assume-filename=untitled.proto`);
+                            break;
+                        case "file": // File is on disk use it's directory
+                            opts['cwd'] = path.dirname(document.uri.fsPath);
+                            args.push(`--assume-filename=${document.uri.fsPath}`);
+                            break;
+                    }
+
+                    let style = vscode.workspace.getConfiguration('clang-format', document).get<string>('style');
+                    style = style && style.trim();
+                    if (style) args.push(`-style=${style}`);
+
+                    let stdout = cp.execFileSync('clang-format', args, opts);
+                    return [new vscode.TextEdit(document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)), stdout ? stdout.toString() : '')];
+                case 'buf':
+                    return document.save().then(() => {
+                        console.log(document.uri.fsPath);
+                        try {
+                            let stdout = cp.execFileSync('buf', ["format", document.uri.fsPath]);
+                            let edit = [new vscode.TextEdit(document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)), stdout ? stdout.toString() : '')];
+                            return Promise.resolve(edit);
+                        } catch (e) {
+
+                        }
+
+                    })
+
             }
 
-            let style = vscode.workspace.getConfiguration('clang-format', document).get<string>('style');
-            style = style && style.trim();
-            if (style) args.push(`-style=${style}`);
 
-            let stdout = cp.execFileSync('clang-format', args, opts);
-            return [new vscode.TextEdit(document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)), stdout ? stdout.toString() : '')];
         },
     });
 }
